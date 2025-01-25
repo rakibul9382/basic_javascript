47ea2388c3cc499f4eccca363401d0ce7f1b5d81
rakibul9382 <skrakibulislam9623@gmail.com>
2025-01-25 09:34:02 +0000

    after editing

--- 001_basics/05_string.js
+++ 001_basics/05_string.js
@@ -5,7 +5,9 @@ console.log(name + repoCount);
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('Rakilbul')
+const gameName = new String('Sk Rakilbul Islam')
+console.log(gameName.length);
+
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -17,6 +19,21 @@ console.log(gameName.indexOf('u'));
 console.log(gameName.lastIndexOf('l'));
 const newString = gameName.substring(1,4)
 console.log(newString);
+const anotherString = gameName.slice(-5)
+console.log(anotherString);
+
+const newStringOne = "     rakibul   "
+console.log(newStringOne.trim());
+
+const url = "https://rakibul.com//tipu%20sultan"
+console.log(url.replace('%20', '_'));
+console.log(url.includes('tipu'));
+console.log(gameName.split());
+console.log(gameName.split(''));
+console.log(gameName.split(' '));
+
+
+
 
 
 
