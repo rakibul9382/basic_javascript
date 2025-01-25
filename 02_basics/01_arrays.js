256651fb5c901b313e826dcbc127469c2bae541e
rakibul9382 <skrakibulislam9623@gmail.com>
2025-01-25 18:20:25 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+//array
+const myArr = [0, 1 , 2 , 3 , 8]
+const myHeroes = ["chair","table","book","laptop"]
+const myArr2 = new Array(1, 2, 3, 4)
+
+// console.log(myArr.length);
+// console.log(myArr2);
+// // array methode
+
+// myArr.push(6)
+// console.log(myArr);
+// myArr.pop() //remove the last element
+// console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+// const  newArray = myArr.join()
+// console.log(myArr);
+// console.log(typeof newArray);
+console.log("a",myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+console.log("b",myArr);
+const myn2 = myArr.splice(1,3)
+console.log("c",myArr);
+console.log(myn2);
+
