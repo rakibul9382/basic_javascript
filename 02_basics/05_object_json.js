973d528e46ffc32007eb9f08141a23ec9732b324
rakibul9382 <skrakibulislam9623@gmail.com>
2025-01-26 15:08:15 +0000

    objest lastpart

---
+++ 02_basics/05_object_json.js
@@ -0,0 +1,7 @@
+const course = {
+    courseName : "java script",
+    price : 999,
+    courseInstructor : "Rakibul"
+}
+const{courseInstructor : instructor} = course //destructuring
+console.log(instructor);
